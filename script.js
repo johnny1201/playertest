d73afc777a06b4ff8678e20c2aa339b6b31c238a
Johnny Michael Alves Faria <johnny.faria1992@gmail.com>
2024-08-09 16:52:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-// Example link to the manifest or video source
+// Example link to the m3u8 manifest
 const videoManifestUrl = 'https://sp.cdnsimba.com.br/bpk-tv/Record01/default/index.m3u8';
 
-// Function to load and play the video
 function loadVideo() {
-    const videoPlayer = document.getElementById('videoPlayer');
-    
-    // Here you would normally process the manifest if necessary,
-    // or directly set the video source if it's already processed.
-    videoPlayer.src = videoManifestUrl;
-    videoPlayer.load();
-    videoPlayer.play();
+    const video = document.getElementById('videoPlayer');
+
+    if (Hls.isSupported()) {
+        const hls = new Hls();
+        hls.loadSource(videoManifestUrl);
+        hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED, function() {
+            video.play();
+        });
+    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+        video.src = videoManifestUrl;
+        video.addEventListener('loadedmetadata', function() {
+            video.play();
+        });
+    } else {
+        alert('Seu navegador não suporta HLS.');
+    }
 }
 
 // Call the function to load and play the video
